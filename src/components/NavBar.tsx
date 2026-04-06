1216148f8ede5ccf4ebb5c60f1c1025d376d1eb6
CODESINS25 <codesinisters@gmail.com>
2026-04-06 14:51:07 +0530

    fixed navbar toggle

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,35 @@
-import { HStack, Image } from '@chakra-ui/react'
+import { HStack, Image, IconButton } from '@chakra-ui/react';
+import { HamburgerIcon } from '@chakra-ui/icons';
 import logo from '../assets/logo.webp';
 import ColorModeSwitch from './ColorModeSwitch';
 import SearchInput from './SearchInput';
 
 interface Props {
   onSearch: (searchText: string) => void;
+  onToggleSidebar?: () => void; // ✅ ADD THIS
 }
 
-const NavBar = ({ onSearch }: Props) => {
+const NavBar = ({ onSearch, onToggleSidebar }: Props) => {
   return (
-    <HStack padding='10px'>
-      <Image src={logo} boxSize='60px' />
+    <HStack padding="10px" gap={3}>
+      
+      {/* 🔥 Sidebar Toggle Button */}
+      <IconButton
+        icon={<HamburgerIcon />}
+        aria-label="Toggle Sidebar"
+        onClick={onToggleSidebar}
+      />
+
+      {/* Logo */}
+      <Image src={logo} boxSize="60px" />
+
+      {/* Search */}
       <SearchInput onSearch={onSearch} />
+
+      {/* Dark Mode */}
       <ColorModeSwitch />
     </HStack>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
